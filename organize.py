68ebfa9bfba7d6d734e77336774dd1d85c3b7f2f
Nick Ahn <n_ahn2@u.pacific.edu>
2022-04-12 17:49:25 -0700

    Added file counter and changed directory names

--- organize.py
+++ organize.py
@@ -1,4 +1,5 @@
 import glob
+from re import I
 from tkinter import W
 import os
 from os import path
@@ -6,13 +7,16 @@ from os import path
 # Directory to Organize
 directory = "/home/nickel/Downloads/"
 
-#File Extensions
+# File Extensions
 extensions = {}
-extensions["documents"] = [".pdf", ".docx", ".doc", ".csv"]
-extensions["pictures"] = [".jpeg", ".jpg", ".png", ".gif"]
-extensions["videos"] = [".mp4", ".avi", ".webm", ".mkv"]
-extensions["audio"] = [".mp3"]
-extensions["compressed"] = [".zip"]
+extensions["Documents"] = [".pdf", ".docx", ".doc", ".csv"]
+extensions["Pictures"] = [".jpeg", ".jpg", ".png", ".gif"]
+extensions["Videos"] = [".mp4", ".avi", ".webm", ".mkv"]
+extensions["Audio"] = [".mp3"]
+extensions["Compressed"] = [".zip"]
+
+# Count number of files moved for each extension
+counter = {}
 
 '''
 for file in DownloadsFolder:
@@ -25,6 +29,19 @@ for file in DownloadsFolder:
 for file in glob.glob(directory + "*"):
     for key in extensions.keys():
         if os.path.splitext(file)[1] in extensions[key]:
-            if path.exists(directory+str(key)):
-                print("-" + directory+str(key))
+            if not path.exists(directory+str(key)):
+                os.makedirs(directory + key)
+
+            count = counter.get(key, 0) + 1  # update count 
+            counter[key] = count
+            print("-" + directory+str(key))
+
+print("Finished organizing", directory, "directory.\nSummary: ")
+total_count = 0
+for key, value in counter.items():
+    total_count += value
+    print("- Moved ", str(value), " items to ", str(key))
+
+print("Moved ", str(total_count), " files in total.")
+
 
